d1a13a97a7301b4540bc8bd01cf29972dd3b0d44
David Wisecarver <dwisecar@gmail.com>
2020-10-22 16:09:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,14 +10,17 @@ def load_library(file_to_load)
   emoticons = YAML.load_file(file_to_load)
   new_emoticons = emoticons.each_with_object do |(key, value), final_hash|
     value.each do |element|
-      
+      binding.pry
+      if !final_hash[key]
+        final_hash[key] = {}
+      end
       if value == value[0]
-        final_hash[key] = {:english => element}
+        key = {:english => element}
       end
       if value == value[1]
-        final_hash[key].merge!(japanese: "element")
+        key.merge!(japanese: "element")
       end
-      binding.pry
+      
     end
     
   end
