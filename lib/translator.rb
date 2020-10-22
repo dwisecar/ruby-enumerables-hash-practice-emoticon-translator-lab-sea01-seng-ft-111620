e834e356e343f031cf406d6a76df4ebb789b240f
David Wisecarver <dwisecar@gmail.com>
2020-10-22 17:27:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,11 +27,12 @@ def get_japanese_emoticon(file, emoticon)
   translated_emoticon = ""
   
   sorted_emoticons.each do |key, value| 
+    binding.pry
     if emoticon == value[:english]
       translated_emoticon = value[:japanese]
     end
   end
-  if translated_emoticon = ""
+  if translated_emoticon == ""
     puts "Sorry, that emoticon was not found"
   end
   translated_emoticon
