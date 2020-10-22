e76ee391d7b2a5430f8ade0cb210220294f9febb
David Wisecarver <dwisecar@gmail.com>
2020-10-22 15:33:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,13 @@
 require "pry"
 require "yaml"
 
-emoticons = YAML.load_file('lib/emoticons.yml')
 #emoticons is now a hash with keys like "angry" that point to arrays with two elements, first is englis emoticon, second is japanese.
 
 
 
 def load_library
   emoticons = YAML.load_file('lib/emoticons.yml')
+  emoticons
 end
 #binding.pry
 def get_japanese_emoticon
