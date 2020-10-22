b13494187a081a0cae47af45a2b00e3310265b0b
David Wisecarver <dwisecar@gmail.com>
2020-10-22 17:25:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,7 +30,7 @@ def get_japanese_emoticon(file, emoticon)
     if emoticon == value[:english]
       translated_emoticon = value[:japanese]
     else
-      puts "Sorry, that emoticon was not found"
+      translated_emoticon = "Sorry, that emoticon was not found"
     end
   end
   translated_emoticon
